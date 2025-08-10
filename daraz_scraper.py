6f3d90bc8b2e4dc41ad4d062cb1e1d8b073d9fd5
MuhammadUsman-10 <usman.aamir404@gmail.com>
2025-08-11 03:04:24 +0500

    workflow updated

--- daraz_scraper.py
+++ daraz_scraper.py
@@ -23,6 +23,7 @@ options.add_argument("--headless=new")  # New headless mode
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
+options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")  # unique temp dir
 
 driver = webdriver.Edge(options=options)
 
